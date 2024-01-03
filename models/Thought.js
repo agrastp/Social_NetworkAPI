ed6e0621557de818c8437caf985d90198e540de9
AgrastP <agrastp@yahoo.com>
2024-01-03 15:48:53 -0500

    file organization and model setup

---
+++ models/Thought.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal),
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [reactionSchema],
+});
+
+// Virtual for reactionCount
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+// Format timestamp function
+function dateFormat(date) {
+    return date.toLocaleString();
+}
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
